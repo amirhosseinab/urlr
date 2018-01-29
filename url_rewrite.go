efe594c7289e404fb3fcc713d1b0c366ca827511
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2018-01-29 13:28:44 +0330

    re-write api
    add Option and Constructor and a method to the object to allow chain
    this handler with other handlers

---
+++ url_rewrite.go
@@ -0,0 +1,52 @@
+package urlrewrite
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type (
+	URLRewrite struct {
+		Options *Options
+	}
+
+	//SchemeHeader: X-Forwarded-Proto
+	Options struct {
+		AcceptHTTP   bool
+		AcceptWWW    bool
+		SchemeHeader string
+	}
+)
+
+func New(opt *Options) *URLRewrite {
+	return &URLRewrite{opt}
+}
+
+func (u *URLRewrite) Handler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rewrite(w, r, u.Options)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func rewrite(w http.ResponseWriter, r *http.Request, opt *Options) {
+	scheme := strings.ToLower(r.Header.Get(opt.SchemeHeader))
+	host := r.Host
+
+	targetScheme := scheme
+	bareHost := strings.TrimPrefix(r.Host, "www.")
+
+	if !opt.AcceptHTTP && scheme == "http" {
+		scheme = "https"
+	}
+
+	if !opt.AcceptWWW && strings.HasPrefix(host, "www") {
+		host = bareHost
+	}
+
+	if scheme != targetScheme || host != bareHost {
+		url := fmt.Sprintf("%v://%v%v", scheme, host, r.RequestURI)
+		http.Redirect(w, r, url, http.StatusPermanentRedirect)
+	}
+}
